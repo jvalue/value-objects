c7919551b44f80fe87962f7f65bef3863fb8066e
Dirk Riehle <dirk@riehle.org>
2010-08-03 14:20:22 +0200

    Fixed up CONTRIBUTIONS, added AllTests class.

---
+++ test/org/jvalue/AllTests.java
@@ -0,0 +1,37 @@
+package org.jvalue;
+
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.junit.runner.*;
+import org.junit.runners.*;
+
+/**
+ * Wolf sagt durch die Brust ins Auge annotiert.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {
+	org.jvalue.names.NameTest.class,
+
+	org.jvalue.numbers.RangeBoundTest.class,
+	org.jvalue.numbers.RangeTest.class,
+
+	org.jvalue.si.QuantityUnitTest.class,
+	org.jvalue.si.SiUnitTest.class
+})
+
+public class AllTests {
+
+}
